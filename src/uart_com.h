5a6d3fdaa37fc375ee0022049b70e913f89a1c00
Ivan Bobovich <ivan@bobovich.ru>
2020-03-22 22:24:07 +0300

    22.03.2020 2223

---
+++ src/uart_com.h
@@ -0,0 +1,26 @@
+/*
+ * uart_com.h
+ *
+ *  Created on: 18 мар. 2020 г.
+ *      Author: Иван
+ */
+
+#ifndef UART_COM_H_
+#define UART_COM_H_
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "cc1101.h"
+
+
+void aTaskUart(void * pvParameters);
+struct pQueueComm
+{
+	QueueHandle_t a1TX;
+	QueueHandle_t a1RX;
+	QueueHandle_t a2TX;
+	QueueHandle_t a2RX;
+};
+
+
+#endif /* UART_COM_H_ */
